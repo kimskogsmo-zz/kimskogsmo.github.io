20feac3b8940bc1716f3a06e3bea6b549f113920
Kim Skogsmo <kim.skogsmo@ancon.io>
2019-12-07 13:27:35 +0100

    Updates

---
+++ precache-manifest.868977e6ff53426dc1fd1efa4abcbdab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5877b3bda30940c3036cdbc768e4e76",
+    "url": "/fifa-leaderboards/index.html"
+  },
+  {
+    "revision": "673348d93b495beef164",
+    "url": "/fifa-leaderboards/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e54a88b3c96a9bc338af",
+    "url": "/fifa-leaderboards/static/js/2.05dada91.chunk.js"
+  },
+  {
+    "revision": "673348d93b495beef164",
+    "url": "/fifa-leaderboards/static/js/main.48c34657.chunk.js"
+  },
+  {
+    "revision": "fe90bbb73b60018024e2",
+    "url": "/fifa-leaderboards/static/js/runtime-main.0399a274.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/fifa-leaderboards/static/media/logo.25bf045c.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/fifa-leaderboards/precache-manifest.868977e6ff53426dc1fd1efa4abcbdab.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fifa-leaderboards/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
